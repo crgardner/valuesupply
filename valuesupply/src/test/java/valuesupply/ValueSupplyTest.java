c7835b9cb40c6651ef6867e900f304359cfaf28a
Christopher Gardner <chris.r.gardner@gmail.com>
2014-03-15 14:58:13 -0400

    Minor changes.

--- valuesupply/src/test/java/valuesupply/ValueSupplyTest.java
+++ valuesupply/src/test/java/valuesupply/ValueSupplyTest.java
@@ -2,8 +2,6 @@ package valuesupply;
 
 import static org.mockito.Mockito.verify;
 
-import java.util.Map;
-
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,7 +9,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ValueSupplyTest {
@@ -54,17 +51,9 @@ public class ValueSupplyTest {
 
         valueSupply.provideAllTo(consumer);
 
-        verify(consumer).accept(httpHeaderCategory, equalTo(aMap().put(arrivalName, helloSupplier)
-                                                                  .put(approachingName, onTheWaySupplier)));
-        verify(consumer).accept(urlComponentCategory, equalTo(aMap().put(departureName, goodbyeSupplier)));
-    }
-
-    private Map<String, Supplier<String>> equalTo(Builder<String, Supplier<String>> map) {
-        return map.build();
-    }
-
-    private ImmutableMap.Builder<String, Supplier<String>> aMap() {
-        return new ImmutableMap.Builder<String, Supplier<String>>();
+        verify(consumer).accept(httpHeaderCategory, ImmutableMap.of(arrivalName, helloSupplier,
+                                                                    approachingName, onTheWaySupplier));
+        verify(consumer).accept(urlComponentCategory, ImmutableMap.of(departureName, goodbyeSupplier));
     }
 
 }
