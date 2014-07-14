b6c62f0863f773dd84a2a985cddd45119a27033f
gardnchr <christopher.gardner@adp.com>
2014-07-14 11:52:49 -0400

    Minor changes.

--- valuesupply/src/main/java/valuesupply/ValueSupply.java
+++ valuesupply/src/main/java/valuesupply/ValueSupply.java
@@ -10,25 +10,28 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
 public class ValueSupply {
-    private final Multimap<ValueSupplyCategory, ValueSupplyItem> experimentalSuppliers = HashMultimap.create();
+    private final Multimap<ValueSupplyCategory, ValueSupplyItem> suppliers = HashMultimap.create();
 
     public void add(ValueSupplyCategory category, String name,
             Supplier<Object> supplier) {
-        experimentalSuppliers.put(category, new ValueSupplyItem(name, supplier));
+        suppliers.put(category, new ValueSupplyItem(name, supplier));
     }
 
     public void supplyAllOf(ValueSupplyCategory category, Consumer<Map<String, Object>> allConsumer) {
-        Map<String, Object> itemsByName = new HashMap<>();
-
-        for (ValueSupplyItem valueSupplyItem : experimentalSuppliers.get(category)) {
-            itemsByName.put(valueSupplyItem.name(), valueSupplyItem.valueAsString());
-        }
-        allConsumer.accept(itemsByName);
+        allConsumer.accept(groupItemsByNameWith(category));
     }
 
-    public void supplyEachOf(ValueSupplyCategory category,
-            Consumer<ValueSupplyItem> eachConsumer) {
-        for (ValueSupplyItem valueSupplyItem : experimentalSuppliers.get(category)) {
+    private Map<String, Object> groupItemsByNameWith(ValueSupplyCategory category) {
+        Map<String, Object> itemsByName = new HashMap<>();
+
+        for (ValueSupplyItem valueSupplyItem : suppliers.get(category)) {
+            itemsByName.put(valueSupplyItem.name(), valueSupplyItem.valueAsString());
+        }
+        return itemsByName;
+    }
+
+    public void supplyEachOf(ValueSupplyCategory category, Consumer<ValueSupplyItem> eachConsumer) {
+        for (ValueSupplyItem valueSupplyItem : suppliers.get(category)) {
             eachConsumer.accept(valueSupplyItem);
         }
     }
